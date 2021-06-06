5ed16ae3d223eababa0ca3a3cc3510b9c8aa436d
gvijayku <gvijayku@umich.edu>
2021-06-06 19:06:45 -0400

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from 'node-fetch'
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    let URL = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${req.body}&aqi=no`
+    let data
+    try {
+      const response = await fetch(URL)
+      data = await response.json()
+    } catch (error) {
+      console.error(error)
+    }
+    if ('error' in data) {
+      return res.status(200).json({ data: 'error' })
+    }
+    return res.status(200).json({ data })
+  } else {
+    return res.status(200).send('running')
+  }
 }
